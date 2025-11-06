d2f1c0af88414db200cccf38a3e5353ee219018d
Ilyasse-ben <ilyassebentaleb7@gmail.com>
2025-11-06 21:39:49 +0100

    ui/ux task

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { SingUp } from './sing-up/sing-up';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"sing", component:Login},
+    {path:"singUp", component:SingUp},
+];
